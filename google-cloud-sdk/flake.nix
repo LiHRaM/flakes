e8af0d0bd5f466743db3a1aee20fe5f6066e5a14
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2025-06-05 11:32:13 +0200

    feat: add gcloud flake

---
+++ google-cloud-sdk/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Google Cloud Platform Developer Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{nixpkgs, flake-parts, ...}: flake-parts.lib.mkFlake {inherit inputs;} {
+    systems = [
+      "aarch64-darwin"
+    ];
+
+    perSystem = {pkgs, system, ...}: {
+      packages.default = pkgs.google-cloud-sdk.withExtraComponents (with pkgs.google-cloud-sdk.components; [
+        gke-gcloud-auth-plugin
+      ]);
+    };
+  };
+}
