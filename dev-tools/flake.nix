d54f227553ceb6013cae9ac79acab37b0d2b52cb
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2025-06-05 12:07:23 +0200

    feat(dev-tools): create

---
+++ dev-tools/flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Generic Developer Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{nixpkgs, flake-parts, ...}: flake-parts.lib.mkFlake {inherit inputs;} {
+    systems = [
+      "aarch64-darwin"
+    ];
+
+    perSystem = {pkgs, system, ...}: {
+      packages.default = pkgs.buildEnv {
+        name = "dev-utils";
+        paths = with pkgs; [
+          # CLI
+          helix
+          yazi
+          watchexec
+          jj
+          fd
+          
+          # Golang
+          go
+          gopls
+
+          # Rust
+          cargo
+          rustc
+
+          # Just
+          just
+          just-lsp
+
+          # Markdown
+          markdown-oxide
+          marksman
+
+          # NodeJS
+          yarn
+
+          # Nix
+          nil
+        ]
+      }
+    };
+  };
+}
