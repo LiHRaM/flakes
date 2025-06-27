f0aaea96a0bd158f49518234d8d47fb73c8d7102
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2025-06-27 13:26:33 +0200

    Revert "feat: use carapace from master branch"
    This reverts commit bbb8e26fc7dbc8b033adad9c284cf5ca7f49b73c.

--- dev-tools/flake.nix
+++ dev-tools/flake.nix
@@ -3,16 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgs-master.url = "github:nixos/nixpkgs?ref=master";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{nixpkgs, nixpkgs-master, flake-parts, ...}: flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs = inputs@{nixpkgs, flake-parts, ...}: flake-parts.lib.mkFlake {inherit inputs;} {
     systems = [
       "aarch64-darwin"
     ];
 
-    perSystem = {pkgs, pkgs-master, system, ...}: {
+    perSystem = {pkgs, system, ...}: {
         
       packages.default = pkgs.buildEnv {
         name = "dev-utils";
@@ -25,7 +24,7 @@
           jujutsu
           fd
           go-task
-          pkgs-master.carapace
+          carapace
           difftastic
           starship
           television
